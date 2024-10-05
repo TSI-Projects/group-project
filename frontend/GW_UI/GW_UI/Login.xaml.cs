1701752e824fc9ddac11fe5f4228bb385d3cc840
Captain Crabber <48887927+narcisiwe@users.noreply.github.com>
2024-10-05 14:33:54 +0300

    Login + MainMenu
    1. Add new login page
    2. Add new MainMenu page
    3. Edit app.xaml

---
+++ frontend/GW_UI/GW_UI/Login.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GW_UI
+{
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            // Проверка логина и пароля
+            if (login == "11111111" && password == "1")
+            {
+                // Открываем главное меню при успешной проверке
+                MainMenu mainMenu = new MainMenu();
+                mainMenu.Show();
+                this.Close(); // Закрываем окно логина
+            }
+            else
+            {
+                // Выводим сообщение об ошибке при неверном логине или пароле
+                MessageBox.Show("Неверный логин или пароль", "Ошибка входа", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
