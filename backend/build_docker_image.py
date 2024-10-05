77be5e16cc7382312ced7d0037175e03a558fcd9
Vadims Pinсuks <skinnoniks@gmail.com>
2024-10-05 12:56:50 +0300

    feat(devops): Expose `containerPort` to values.yaml (#29)
    * fix build args
    
    * add `port` build arg
    
    * expose `containerPort` to `values.yaml`

--- backend/build_docker_image.py
+++ backend/build_docker_image.py
@@ -4,6 +4,7 @@ import subprocess
 def build_docker_image(
     name: str = "simple_go_server",
     tag: str = "latest",
+    port: int = 8000,
     dockerfile_dir: str = ".",
     source_dir: str = ".",
     maingo_file_path: str = "./cmd/app/main.go",
@@ -11,8 +12,9 @@ def build_docker_image(
     subprocess.call([
         "docker", "build", dockerfile_dir,
         "-t", f"{name}:{tag}",
-        "--build-arg", f"SOURCE_DIR={source_dir}",
-        "--build-arg", f"MAINGO_FILE_PATH={maingo_file_path}",
+        "--build-arg", f"GO_SERVER_PORT={port}",
+        "--build-arg", f"GO_SERVER_DIR={source_dir}",
+        "--build-arg", f"GO_SERVER_MAIN_PATH={maingo_file_path}",
     ])
 
 if __name__ == "__main__":
