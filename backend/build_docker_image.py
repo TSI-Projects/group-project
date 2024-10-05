8f0d07c2300c9e18bc376e9de1944e65078dc696
Vadims Pincuks <skinnoniks@gmail.com>
2024-10-05 10:01:51 +0300

    add `port` build arg

--- backend/build_docker_image.py
+++ backend/build_docker_image.py
@@ -4,6 +4,7 @@ import subprocess
 def build_docker_image(
     name: str = "simple_go_server",
     tag: str = "latest",
+    port: int = 8000,
     dockerfile_dir: str = ".",
     source_dir: str = ".",
     maingo_file_path: str = "./cmd/app/main.go",
@@ -11,6 +12,7 @@ def build_docker_image(
     subprocess.call([
         "docker", "build", dockerfile_dir,
         "-t", f"{name}:{tag}",
+        "--build-arg", f"GO_SERVER_PORT={port}",
         "--build-arg", f"GO_SERVER_DIR={source_dir}",
         "--build-arg", f"GO_SERVER_MAIN_PATH={maingo_file_path}",
     ])
