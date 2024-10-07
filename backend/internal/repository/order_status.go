3aa64a3880da38442f8bc5d18a1ce22252ec029c
Andrejs Ponatovskis <andrejs.pon00@gmail.com>
2024-10-07 19:42:53 +0300

    Change OrderStatus struct fields to appropriate types to prevent crashes

--- backend/internal/repository/order_status.go
+++ backend/internal/repository/order_status.go
@@ -1,10 +1,12 @@
 package repository
 
+import "time"
+
 type OrderStatus struct {
-	ID                 int    `db:"id"`
-	ReadyAt            string `db:"ready_at"`
-	ReturnedAt         string `db:"returned_at"`
-	CustomerNotifiedAt string `db:"customer_notified_at"`
-	IsOutsourced       bool   `db:"is_outsourced"`
-	IsReceiptLost      bool   `db:"is_recipient"`
+	ID                 int        `db:"id"`
+	ReadyAt            *time.Time `db:"ready_at"`
+	ReturnedAt         *time.Time `db:"returned_at"`
+	CustomerNotifiedAt *time.Time `db:"customer_notified_at"`
+	IsOutsourced       bool       `db:"is_outsourced"`
+	IsReceiptLost      bool       `db:"is_recipient"`
 }
