2d7f9ca4f44779b3c0ea76b87957ff90e3567541
Andrejs Ponatovskis <93515289+AndrejsPon00@users.noreply.github.com>
2024-10-07 21:15:21 +0300

    Add API and DB methods for order_type table (#41)
    * Add DB methods for order_type table
    
    * Add order type handler to API

---
+++ backend/internal/api/handlers/order_type.go
@@ -0,0 +1,70 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/TSI-Projects/group-project/internal/repository"
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+)
+
+func (h *Handler) GetOrderTypes(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	orderTypes, err := h.OrderTypeRepo.GetOrderTypes()
+	if err != nil {
+		log.Errorf("failed to get order types: %v", err)
+		w.Write([]byte("Internal Error"))
+		return
+	}
+
+	output, err := json.Marshal(orderTypes)
+	if err != nil {
+		log.Errorf("failed to marshal order types: %v", err)
+		w.Write([]byte("Internal Error"))
+		return
+	}
+
+	w.Write(output)
+}
+
+func (h *Handler) CreateOrderType(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	var orderType repository.OrderType
+
+	if err := json.NewDecoder(r.Body).Decode(&orderType); err != nil {
+		log.Errorf("failed to decode body: %v", err)
+		w.Write([]byte("Internal Error"))
+		return
+	}
+
+	if err := h.OrderTypeRepo.CreateOrderType(&orderType); err != nil {
+		log.Errorf("failed to create order type: %v", err)
+		w.Write([]byte("Internal Error"))
+		return
+	}
+
+	w.Write([]byte("Success"))
+}
+
+func (h *Handler) DeleteOrderType(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	rawId := mux.Vars(r)["id"]
+	id, err := strconv.Atoi(rawId)
+	if err != nil {
+		log.Errorf("failed to convert str id to int: id '%s': %v ", rawId, err)
+		w.Write([]byte("Internal Error"))
+		return
+	}
+
+	if err := h.OrderTypeRepo.DeleteOrderType(id); err != nil {
+		log.Errorf("failed to delete order type item: %s", err)
+		w.Write([]byte("Internal Error"))
+		return
+	}
+
+	w.Write([]byte("Success"))
+}
