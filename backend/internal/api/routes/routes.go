e7d49d3bb53a2441a92faeed52577671faf58515
Andrejs Ponatovskis <andrejs.pon00@gmail.com>
2024-10-07 19:41:12 +0300

    Add API routes for orders, workers, languages, and their types

--- backend/internal/api/routes/routes.go
+++ backend/internal/api/routes/routes.go
@@ -12,7 +12,22 @@ func NewRouter(dbClient db.IDatabase) *mux.Router {
 	router := mux.NewRouter()
 	handler := handlers.NewHandler(dbClient)
 
-	router.HandleFunc("/api/order", handler.GetOrder).Methods(http.MethodGet)
-	router.HandleFunc("/", handler.GetOrder).Methods(http.MethodGet)
+	router.HandleFunc("/api/orders", handler.GetOrders).Methods(http.MethodGet)
+	router.HandleFunc("/api/orders", handler.CreateOrder).Methods(http.MethodPost)
+	router.HandleFunc("/api/order/{id}", handler.DeleteOrder).Methods(http.MethodDelete)
+
+	router.HandleFunc("/api/orders/types", handler.GetOrderTypes).Methods(http.MethodGet)
+	router.HandleFunc("/api/orders/types", handler.CreateOrderType).Methods(http.MethodPost)
+	router.HandleFunc("/api/orders/type/{id}", handler.DeleteOrderType).Methods(http.MethodDelete)
+
+	router.HandleFunc("/api/workers", handler.GetWorkers).Methods(http.MethodGet)
+	router.HandleFunc("/api/workers", handler.CreateWorker).Methods(http.MethodPost)
+	router.HandleFunc("/api/worker/{id}", handler.DeleteWorker).Methods(http.MethodDelete)
+
+	router.HandleFunc("/api/languages", handler.GetLanguages).Methods(http.MethodGet)
+	router.HandleFunc("/api/languages", handler.CreateLanguage).Methods(http.MethodPost)
+	router.HandleFunc("/api/language/{id}", handler.DeleteLanguage).Methods(http.MethodDelete)
+
+	router.HandleFunc("/", handler.GetOrders).Methods(http.MethodGet)
 	return router
 }
