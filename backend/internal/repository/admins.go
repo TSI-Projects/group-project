3185789312da94d14650b0c24bb9b56b1abf0ac2
Andrejs Ponatovskis <andrejs.pon00@gmail.com>
2024-10-08 20:04:40 +0300

    Add JSON tags to database entity structs

--- backend/internal/repository/admins.go
+++ backend/internal/repository/admins.go
@@ -1,7 +1,7 @@
 package repository
 
 type Admin struct {
-	ID       int    `db:"id"`
-	Username string `db:"username"`
-	Password string `db:"password"`
+	ID       int    `db:"id"          json:"id"`
+	Username string `db:"username"    json:"username"`
+	Password string `db:"password"    json:"password"`
 }
--- backend/internal/repository/order_status.go
+++ backend/internal/repository/order_status.go
@@ -3,10 +3,10 @@ package repository
 import "time"
 
 type OrderStatus struct {
-	ID                 int        `db:"id"`
-	ReadyAt            *time.Time `db:"ready_at"`
-	ReturnedAt         *time.Time `db:"returned_at"`
-	CustomerNotifiedAt *time.Time `db:"customer_notified_at"`
-	IsOutsourced       bool       `db:"is_outsourced"`
-	IsReceiptLost      bool       `db:"is_recipient"`
+	ID                 int        `db:"id"                       json:"id"`
+	ReadyAt            *time.Time `db:"ready_at"                 json:"ready_at"`
+	ReturnedAt         *time.Time `db:"returned_at"              json:"returned_at"`
+	CustomerNotifiedAt *time.Time `db:"customer_notified_at"     json:"customer_notified_at"`
+	IsOutsourced       bool       `db:"is_outsourced"            json:"is_outsourced"`
+	IsReceiptLost      bool       `db:"is_recipient_lost"        json:"is_recipient_lost"`
 }
