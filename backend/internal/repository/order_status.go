0793ba79722fa0de971671b3c41af908e8d38ac1
Andrejs Ponatovskis <andrejs.pon00@gmail.com>
2024-10-11 12:54:32 +0300

    Add validation tags for OrderStatus structure

--- backend/internal/repository/order_status.go
+++ backend/internal/repository/order_status.go
@@ -3,10 +3,10 @@ package repository
 import "time"
 
 type OrderStatus struct {
-	ID                 int        `db:"id"                       json:"id"`
-	ReadyAt            *time.Time `db:"ready_at"                 json:"ready_at"`
-	ReturnedAt         *time.Time `db:"returned_at"              json:"returned_at"`
-	CustomerNotifiedAt *time.Time `db:"customer_notified_at"     json:"customer_notified_at"`
-	IsOutsourced       bool       `db:"is_outsourced"            json:"is_outsourced"`
-	IsReceiptLost      bool       `db:"is_recipient_lost"        json:"is_recipient_lost"`
+	ID                 int        `db:"id"                       json:"id"                     validate:"omitempty"`
+	ReadyAt            *time.Time `db:"ready_at"                 json:"ready_at"               validate:"omitempty"`
+	ReturnedAt         *time.Time `db:"returned_at"              json:"returned_at"            validate:"omitempty"`
+	CustomerNotifiedAt *time.Time `db:"customer_notified_at"     json:"customer_notified_at"   validate:"omitempty"`
+	IsOutsourced       bool       `db:"is_outsourced"            json:"is_outsourced"          validate:"omitempty"`
+	IsReceiptLost      bool       `db:"is_recipient_lost"        json:"is_recipient_lost"      validate:"omitempty"`
 }
