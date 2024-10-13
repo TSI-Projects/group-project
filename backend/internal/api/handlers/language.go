6e2ddf764f2a42728c27a4cae4db0c2ccea81747
Andrejs Ponatovskis <andrejs.pon00@gmail.com>
2024-10-13 15:53:12 +0300

    Add getByID and update methods for all repos in handler

--- backend/internal/api/handlers/language.go
+++ backend/internal/api/handlers/language.go
@@ -87,3 +87,62 @@ func (h *Handler) DeleteLanguage(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("Success"))
 }
+
+func (h *Handler) GetLanguageByID(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	rawId := mux.Vars(r)["id"]
+	if len(rawId) == 0 {
+		log.Errorf("failed to delete language due to empty id param")
+		w.Write([]byte("Language ID is not specified"))
+		return
+	}
+
+	id, err := strconv.Atoi(rawId)
+	if err != nil {
+		log.Errorf("failed to convert raw str id to int: %v", err)
+		w.Write([]byte("Internal Error"))
+		return
+	}
+
+	language, err := h.LanguageRepo.GetByID(id)
+	if err != nil {
+		log.Errorf("failed to delete language: %v", err)
+		w.Write([]byte("Internal Error"))
+		return
+	}
+
+	output, err := json.Marshal(language)
+	if err != nil {
+		log.Errorf("Failed to marshal languages struct: %v", err)
+		w.Write([]byte("Internal Error"))
+		return
+	}
+
+	w.Write(output)
+}
+
+func (h *Handler) UpdateLanguage(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	language := &repository.Language{}
+
+	if err := json.NewDecoder(r.Body).Decode(language); err != nil {
+		log.Errorf("failed to decode body: %v", err)
+		w.Write([]byte("Internal Error"))
+		return
+	}
+
+	if err := h.Validator.Validate(language); err != nil {
+		log.Errorf("failed to validate language struct: %v", err)
+		w.Write([]byte(utils.UppercaseFirstLetter(err.Error())))
+		return
+	}
+
+	if err := h.LanguageRepo.Update(language); err != nil {
+		log.Errorf("failed to create language: %v", err)
+		w.Write([]byte("Internal Error"))
+		return
+	}
+
+	w.Write([]byte("Success"))
+}
--- backend/internal/api/handlers/worker.go
+++ backend/internal/api/handlers/worker.go
@@ -87,3 +87,62 @@ func (h *Handler) CreateWorker(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("Success"))
 }
+
+func (h *Handler) GetWorkerByID(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	rawId := mux.Vars(r)["id"]
+	if len(rawId) == 0 {
+		log.Errorf("failed to delete worker due to empty id param")
+		w.Write([]byte("Worker ID is not specified"))
+		return
+	}
+
+	id, err := strconv.Atoi(rawId)
+	if err != nil {
+		log.Errorf("failed to convert raw str id to int: %v", err)
+		w.Write([]byte("Internal Error"))
+		return
+	}
+
+	worker, err := h.WorkerRepo.GetByID(id)
+	if err != nil {
+		log.Errorf("failed to delete worker: %v", err)
+		w.Write([]byte("Internal Error"))
+		return
+	}
+
+	output, err := json.Marshal(worker)
+	if err != nil {
+		log.Errorf("Failed to marshal workers struct: %v", err)
+		w.Write([]byte("Internal Error"))
+		return
+	}
+
+	w.Write(output)
+}
+
+func (h *Handler) UpdateWorker(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	worker := &repository.Worker{}
+
+	if err := json.NewDecoder(r.Body).Decode(worker); err != nil {
+		log.Errorf("failed to decode body: %v", err)
+		w.Write([]byte("Internal Error"))
+		return
+	}
+
+	if err := h.Validator.Validate(worker); err != nil {
+		log.Errorf("failed to validate worker struct: %v", err)
+		w.Write([]byte(utils.UppercaseFirstLetter(err.Error())))
+		return
+	}
+
+	if err := h.WorkerRepo.Update(worker); err != nil {
+		log.Errorf("failed to create worker: %v", err)
+		w.Write([]byte("Internal Error"))
+		return
+	}
+
+	w.Write([]byte("Success"))
+}
