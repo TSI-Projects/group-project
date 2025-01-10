6e7219b804af303d9a376795eed862cd06a16112
Andrejs Ponatovskis <93515289+AndrejsPon00@users.noreply.github.com>
2025-01-10 18:22:09 +0200

    Add endpoints for active and completed orders (#71)
    * Add endpoints for active and completed orders
    
    * Fix order update handler

--- backend/internal/repository/order.go
+++ backend/internal/repository/order.go
@@ -32,7 +32,7 @@ type OrderRepo struct {
 	DBClient db.IDatabase
 }
 
-func NewOrderRepo(database db.IDatabase) IRepository[Order] {
+func NewOrderRepo(database db.IDatabase) IOrderRepository[Order] {
 	return &OrderRepo{
 		DBClient: database,
 	}
@@ -115,49 +115,43 @@ func (o *OrderRepo) Delete(id uint) error {
 }
 
 func (o *OrderRepo) GetAll() ([]*Order, error) {
-	orders := make([]*Order, 0)
-
 	selectQuery := `
-	SELECT
-		o.id AS order_id,
-		o.order_status_id,
-		o.order_type_id,
-		o.worker_id,
-		o.customer_id,
-		o.item_name,
-		o.reason,
-		o.defect,
-		o.total_price_eur,
-		o.prepayment_eur,
-		o.created_at,
-		os.id AS order_status_id,
-		os.ready_at,
-		os.returned_at,
-		os.customer_notified_at,
-		os.is_outsourced,
-		os.is_receipt_lost,
-		ot.id AS order_types_id,
-		ot.full_name,
-		c.id AS customer_id,
-		c.language_id,
-		c.phone_number,
-		w.id AS worker_id,
-		w.first_name,
-		w.last_name
-	FROM
-		orders o
-	JOIN
-		order_statuses os
-		ON o.order_status_id = os.id
-	JOIN
-		order_types ot
-		ON o.order_type_id = ot.id
-	JOIN
-		customers c
-		ON o.customer_id = c.id
-	JOIN
-		workers w
-		ON o.worker_id = w.id;`
+    SELECT
+        o.id AS order_id,
+        o.order_status_id,
+        o.order_type_id,
+        o.worker_id,
+        o.customer_id,
+        o.item_name,
+        o.reason,
+        o.defect,
+        o.total_price_eur,
+        o.prepayment_eur,
+        o.created_at,
+        os.id AS order_status_id,
+        os.ready_at,
+        os.returned_at,
+        os.customer_notified_at,
+        os.is_outsourced,
+        os.is_receipt_lost,
+        ot.id AS order_types_id,
+        ot.full_name,
+        c.id AS customer_id,
+        c.language_id,
+        c.phone_number,
+        w.id AS worker_id,
+        w.first_name,
+        w.last_name
+    FROM
+        orders o
+    JOIN
+        order_statuses os ON o.order_status_id = os.id
+    JOIN
+        order_types ot ON o.order_type_id = ot.id
+    JOIN
+        customers c ON o.customer_id = c.id
+    JOIN
+        workers w ON o.worker_id = w.id;`
 
 	rows, err := o.DBClient.Query(selectQuery)
 	if err != nil {
@@ -165,50 +159,15 @@ func (o *OrderRepo) GetAll() ([]*Order, error) {
 	}
 	defer rows.Close()
 
-	for rows.Next() {
-		order := &Order{
-			Status:   &OrderStatus{},
-			Worker:   &Worker{},
-			Customer: &Customer{},
-			Type:     &OrderType{},
-		}
-
-		if err := rows.Scan(
-			&order.ID,
-			&order.OrderStatusID,
-			&order.OrderTypeID,
-			&order.WorkerID,
-			&order.CustomerID,
-			&order.ItemName,
-			&order.Reason,
-			&order.Defect,
-			&order.TotalPrice,
-			&order.Prepayment,
-			&order.CreatedAt,
-			&order.Status.ID,
-			&order.Status.ReadyAt,
-			&order.Status.ReturnedAt,
-			&order.Status.CustomerNotifiedAt,
-			&order.Status.IsOutsourced,
-			&order.Status.IsReceiptLost,
-			&order.Type.ID,
-			&order.Type.FullName,
-			&order.Customer.ID,
-			&order.Customer.LanguageID,
-			&order.Customer.PhoneNumber,
-			&order.Worker.ID,
-			&order.Worker.FirstName,
-			&order.Worker.LastName,
-		); err != nil {
-			return nil, fmt.Errorf("failed to scan row in orders table: %v", err)
-		}
-		orders = append(orders, order)
+	orders, err := scanOrders(rows)
+	if err != nil {
+		return nil, err
 	}
-	fmt.Printf("Fetched Order: %+v\n", orders)
+
 	return orders, nil
 }
 
-func (l *OrderRepo) GetByID(id uint) (*Order, error) {
+func (o *OrderRepo) GetByID(id uint) (*Order, error) {
 	order := &Order{
 		ID:       id,
 		Status:   &OrderStatus{},
@@ -217,7 +176,7 @@ func (l *OrderRepo) GetByID(id uint) (*Order, error) {
 		Type:     &OrderType{},
 	}
 
-	if err := l.DBClient.QueryRow(
+	if err := o.DBClient.QueryRow(
 		`SELECT
 			o.id AS order_id,
 			o.order_status_id,
@@ -292,14 +251,20 @@ func (l *OrderRepo) GetByID(id uint) (*Order, error) {
 	return order, nil
 }
 
-func (l *OrderRepo) Update(order *Order) error {
-	if _, err := l.DBClient.Exec(
-		`UPDATE orders
-		SET reason = $1,
-			defect = $2,
-			total_price_eur = $3,
-			prepayment_eur = $4
-		WHERE id = $5 ;`,
+func (o *OrderRepo) Update(order *Order) error {
+	if _, err := o.DBClient.Exec(
+		`UPDATE orders 
+		SET order_type_id   = $1,
+			worker_id       = $2,
+			item_name       = $3,
+			reason          = $4,
+			defect          = $5,
+			total_price_eur = $6,
+			prepayment_eur  = $7
+		WHERE id = $8;`,
+		order.OrderTypeID,
+		order.WorkerID,
+		order.ItemName,
 		order.Reason,
 		order.Defect,
 		order.TotalPrice,
@@ -308,6 +273,157 @@ func (l *OrderRepo) Update(order *Order) error {
 	); err != nil {
 		return fmt.Errorf("failed to make exec request: %w", err)
 	}
-
 	return nil
+}
+
+func (o *OrderRepo) GetActiveOrders() ([]*Order, error) {
+	selectQuery := `
+    SELECT
+        o.id AS order_id,
+        o.order_status_id,
+        o.order_type_id,
+        o.worker_id,
+        o.customer_id,
+        o.item_name,
+        o.reason,
+        o.defect,
+        o.total_price_eur,
+        o.prepayment_eur,
+        o.created_at,
+        os.id AS order_status_id,
+        os.ready_at,
+        os.returned_at,
+        os.customer_notified_at,
+        os.is_outsourced,
+        os.is_receipt_lost,
+        ot.id AS order_types_id,
+        ot.full_name,
+        c.id AS customer_id,
+        c.language_id,
+        c.phone_number,
+        w.id AS worker_id,
+        w.first_name,
+        w.last_name
+    FROM
+        orders o
+    JOIN
+        order_statuses os ON o.order_status_id = os.id
+    JOIN
+        order_types ot ON o.order_type_id = ot.id
+    JOIN
+        customers c ON o.customer_id = c.id
+    JOIN
+        workers w ON o.worker_id = w.id
+    WHERE
+        os.ready_at IS NULL;`
+
+	rows, err := o.DBClient.Query(selectQuery)
+	if err != nil {
+		return nil, fmt.Errorf("failed to query active orders: %v", err)
+	}
+	defer rows.Close()
+
+	orders, err := scanOrders(rows)
+	if err != nil {
+		return nil, err
+	}
+
+	return orders, nil
+}
+
+func (o *OrderRepo) GetCompletedOrders() ([]*Order, error) {
+	selectQuery := `
+    SELECT
+        o.id AS order_id,
+        o.order_status_id,
+        o.order_type_id,
+        o.worker_id,
+        o.customer_id,
+        o.item_name,
+        o.reason,
+        o.defect,
+        o.total_price_eur,
+        o.prepayment_eur,
+        o.created_at,
+        os.id AS order_status_id,
+        os.ready_at,
+        os.returned_at,
+        os.customer_notified_at,
+        os.is_outsourced,
+        os.is_receipt_lost,
+        ot.id AS order_types_id,
+        ot.full_name,
+        c.id AS customer_id,
+        c.language_id,
+        c.phone_number,
+        w.id AS worker_id,
+        w.first_name,
+        w.last_name
+    FROM
+        orders o
+    JOIN
+        order_statuses os ON o.order_status_id = os.id
+    JOIN
+        order_types ot ON o.order_type_id = ot.id
+    JOIN
+        customers c ON o.customer_id = c.id
+    JOIN
+        workers w ON o.worker_id = w.id
+    WHERE
+        os.ready_at IS NOT NULL;`
+	rows, err := o.DBClient.Query(selectQuery)
+	if err != nil {
+		return nil, fmt.Errorf("failed to query completed orders: %v", err)
+	}
+	defer rows.Close()
+
+	orders, err := scanOrders(rows)
+	if err != nil {
+		return nil, err
+	}
+
+	return orders, nil
+}
+
+func scanOrders(rows *sql.Rows) ([]*Order, error) {
+	orders := make([]*Order, 0)
+	for rows.Next() {
+		order := &Order{
+			Status:   &OrderStatus{},
+			Worker:   &Worker{},
+			Customer: &Customer{},
+			Type:     &OrderType{},
+		}
+		if err := rows.Scan(
+			&order.ID,
+			&order.OrderStatusID,
+			&order.OrderTypeID,
+			&order.WorkerID,
+			&order.CustomerID,
+			&order.ItemName,
+			&order.Reason,
+			&order.Defect,
+			&order.TotalPrice,
+			&order.Prepayment,
+			&order.CreatedAt,
+			&order.Status.ID,
+			&order.Status.ReadyAt,
+			&order.Status.ReturnedAt,
+			&order.Status.CustomerNotifiedAt,
+			&order.Status.IsOutsourced,
+			&order.Status.IsReceiptLost,
+			&order.Type.ID,
+			&order.Type.FullName,
+			&order.Customer.ID,
+			&order.Customer.LanguageID,
+			&order.Customer.PhoneNumber,
+			&order.Worker.ID,
+			&order.Worker.FirstName,
+			&order.Worker.LastName,
+		); err != nil {
+			return nil, fmt.Errorf("failed to scan row in orders table: %v", err)
+		}
+		orders = append(orders, order)
+	}
+	return orders, nil
 }
