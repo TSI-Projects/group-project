9bea724307d243ee99d4e225892c9b4c815c1210
Andrejs Ponatovskis <andrejs.pon00@gmail.com>
2025-01-13 13:47:32 +0200

    Refactor auth middleware to apply new API response structure

--- backend/internal/api/middleware/authorization.go
+++ backend/internal/api/middleware/authorization.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	token "github.com/TSI-Projects/group-project/internal/auth/access_token"
+	response "github.com/TSI-Projects/group-project/internal/models/responses"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -12,15 +13,15 @@ func AuthMW(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 		if len(authHeader) == 0 {
-			log.Errorln("Failed to pass authorization due to missing authorization header")
-			http.Error(w, "Missing Authorization header", http.StatusUnauthorized)
+			log.Error("Failed to pass authorization due to missing authorization header")
+			response.WriteResponseWithError(w, "MISSING_HEADER", "Missing Authorization header", http.StatusBadRequest)
 			return
 		}
 
 		accessToken := strings.TrimPrefix(authHeader, "Bearer ")
 		if _, err := token.Validate(accessToken); err != nil {
 			log.Errorf("Access Token: '%s', Failed to validate access token:'%v'", accessToken, err)
-			http.Error(w, "Invalid or expired token", http.StatusUnauthorized)
+			response.WriteResponseWithError(w, "INVALID_TOKEN", "Invalid or expired token", http.StatusBadRequest)
 			return
 		}
 
